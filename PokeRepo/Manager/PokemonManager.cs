20e053494a58982dd7d4ac16114ab3d76708922c
druzhee <mohannadsa54@gmail.com>
2024-02-07 13:18:19 +0100

    albin

---
+++ PokeRepo/Manager/PokemonManager.cs
@@ -0,0 +1,12 @@
+﻿using PokeRepo.Models;
+
+namespace PokeRepo.Manager
+{
+    public static class PokemonManager
+    {
+        public static List<Root> pokemons { get; set; } = new List<Root>();
+
+      
+
+    }
+}

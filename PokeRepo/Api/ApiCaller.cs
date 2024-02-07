a5b2ff1d7aade58c3980de8622990f89be46e6f8
Adirsflo <rida.abdal@gmail.com>
2024-02-07 11:34:59 +0100

    API & Callers-commit

---
+++ PokeRepo/Api/ApiCaller.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using PokeRepo.Models;
+
+namespace PokeRepo.Api
+{
+    public class ApiCaller
+    {
+        public HttpClient Client { get; set; }
+
+        public ApiCaller()
+        {
+            Client = new();
+
+            Client.BaseAddress = new Uri("https://pokeapi.co/");
+        }
+
+        public async Task<Root> MakeCall(string Url)
+        {
+            HttpResponseMessage response = await Client.GetAsync(Url);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+
+                Root? result = JsonConvert.DeserializeObject<Root>(json);
+
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            throw new HttpRequestException("Could not deserialize Json");
+        }
+    }
+}
